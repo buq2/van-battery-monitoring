ec94c1b53ad639db2a9ae110ddd41e4b87290fdd
Matti Jukola <buq2@buq2.com>
2020-12-25 16:09:31 +0100

    Add buttons to change viewed component

--- android/van_battery_monitor/app/src/main/java/com/buq2/van_battery_monitor/MainActivity.kt
+++ android/van_battery_monitor/app/src/main/java/com/buq2/van_battery_monitor/MainActivity.kt
@@ -14,6 +14,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.webkit.WebView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -31,7 +32,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
         initBle()
         initWebview()
     }
