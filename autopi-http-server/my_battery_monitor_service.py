399d248e7b8c3c5c7e79aa496cbe3da7799cfd5a
Matti Jukola <buq2@buq2.com>
2021-03-13 21:53:51 +0100

    Add check for too old data

--- autopi-http-server/my_battery_monitor_service.py
+++ autopi-http-server/my_battery_monitor_service.py
@@ -19,16 +19,28 @@ users = {
 flask_settings = {
     "host": "0.0.0.0",
     "port": 8000,
-    "threaded": False,
+    "threaded": True,
 }
 
 
 def get_current_status():
     db = shelve.open('/tmp/battery_status.shv')
     if "status" in db:
-        return json.dumps(db['status'])
+        # Check that timestamp is not too old
+        if "timestamp" in db['status']:
+            ts = db['status']['timestamp']
+            if time.time() - ts > 60*5:
+                # Last value was more than 5 minutes ago,
+                # this data is no more valid
+                return None
+
+        return db['status']
     else:
-        return json.dumps({"status":"empty_db"})
+        return {"status": "empty_db"}
+
+
+def get_current_status_json():
+    return json.dumps(get_current_status())
 
 
 @auth.verify_password
@@ -50,7 +62,7 @@ def handle_error(e):
 @app.route('/')
 @auth.login_required
 def index():
-    return get_current_status()
+    return get_current_status_json()
 
 
 @app.route('/api/update_battery_status', methods=['GET', 'POST'])
